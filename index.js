4f8f6a66442d6bc680dc876ba611dc40a5ba851d
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-29 11:43:12 +0100

    -

--- index.js
+++ index.js
@@ -21,14 +21,33 @@ async function ping() {
 
 
 // create user
-async function createUser(JWT) {
-    return axios({
-      method: 'POST',
-      url: 'https://server.othent.io/create-user',
-      data: { JWT }
+async function createUser() {
+
+    const auth0Client = await createAuth0Client({
+        domain: "othent.us.auth0.com",
+        clientId: "dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C"
+    });
+    
+    const options = {
+        authorizationParams: {
+            transaction_input: JSON.stringify({
+                othentFunction: "initializeContract", 
+                warpData: {function: 'initializeContract', data: null}
+            })
+        }
+    };
+    await auth0Client.loginWithPopup(options);
+    const accessToken = await auth0Client.getTokenSilently({
+        detailedResponse: true
+    });
+    const JWT = accessToken.id_token;
+
+    await axios({
+        method: 'POST',
+        url: 'https://server.othent.io/create-user',
+        data: { JWT }
     })
     .then(response => {
-        console.log(response);
         return response.data;
     })
     .catch(error => {
@@ -63,37 +82,32 @@ async function logOut() {
 
 
 
-// sign transaction, sort this one out
-async function signTransaction(othentFunction, ) {
+
+
+
+// sign transaction
+async function signTransaction(toContractId, toContractFunction, txnData) {
 
     const auth0Client = await createAuth0Client({
         domain: "othent.us.auth0.com",
         clientId: "dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C"
-      });
+    });
     
     const options = {
         authorizationParams: {
             transaction_input: JSON.stringify({
-
-            // // create user for warp
-            // othentFunction: "initializeContract", 
-            // warpData: {function: 'initializeContract', data: null},
-
-            // send txn for warp
-            othentFunction: "broadcastTxn",
-            warpData: {
-            function: 'broadcastTxn', 
-            data: {
-                toContractId: 'XL_AtkccUxD45_Be76Qe_lSt8q9amgEO9OQnhIo-2xI',
-                toContractFunction: 'createPost',
-                txnData: {blog_post_1: 'Hello World!'}
-            }},
-
-            // arweave TXN JWT
-            // here
-        })
-    }
-};
+                othentFunction: "broadcastTxn",
+                warpData: {
+                    function: 'broadcastTxn', 
+                    data: {
+                        toContractId: toContractId,
+                        toContractFunction: toContractFunction,
+                        txnData: txnData
+                    }
+                },
+            })
+        }
+    };
     await auth0Client.loginWithPopup(options);
     const accessToken = await auth0Client.getTokenSilently({
         detailedResponse: true
@@ -101,6 +115,8 @@ async function signTransaction(othentFunction, ) {
     const idToken = accessToken.id_token;
     return idToken
 }
+
+
 
 
 
@@ -113,13 +129,12 @@ async function sendTransaction(JWT) {
         data: { JWT }
       })
       .then(response => {
-          console.log(response);
-          return response.data;
-      })
-      .catch(error => {
-          console.log(error.response.data);
-          throw error;
-      });
+        return response.data;
+    })
+    .catch(error => {
+        console.log(error.response.data);
+        throw error;
+    });
 }
 
 
@@ -132,13 +147,12 @@ async function uploadData(file, fileName, fileType) {
         data: { file, fileName, fileType }
       })
       .then(response => {
-          console.log(response);
-          return response.data;
-      })
-      .catch(error => {
-          console.log(error.response.data);
-          throw error;
-        });
+        return response.data;
+    })
+    .catch(error => {
+        console.log(error.response.data);
+        throw error;
+    });
 }
 
 
@@ -152,33 +166,31 @@ async function queryUser(unique_id) {
         data: { unique_id }
       })
       .then(response => {
-          console.log(response);
-          return response.data;
-      })
-      .catch(error => {
-          console.log(error.response.data);
-          throw error;
-        });
+        return response.data;
+    })
+    .catch(error => {
+        console.log(error.response.data);
+        throw error;
+    });
 }
 
 
 
 
 // backup keyfile
-async function backupKeyfile(PEM_public_key) {
+async function backupKeyfile(signedPEMkey) {
     return axios({
         method: 'POST',
         url: 'https://server.othent.io/backup-keyfile',
-        data: { PEM_public_key }
+        data: { signedPEMkey }
       })
       .then(response => {
-          console.log(response);
-          return response.data;
-      })
-      .catch(error => {
-          console.log(error.response.data);
-          throw error;
-        });
+        return response.data;
+    })
+    .catch(error => {
+        console.log(error.response.data);
+        throw error;
+    });
 }
 
 
