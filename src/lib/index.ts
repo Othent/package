bcfae46a3cc8e5de8ce853d134d5a31d670c4924
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-05 18:03:39 -0400

    chore: TODO

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -56,7 +56,7 @@ export async function Othent(params: Types.useOthentProps): Promise<Types.useOth
         throw new Error('Please specify a API Key and API ID (you can get one from Othent.io)')
     }
 
-    // instead add to server later
+    // instead add to server later, auth0 API ID in JWT ?
 
 
 
