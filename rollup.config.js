4834b6ca9ec7b7183454eee54abbe1c62744edcd
7i7o <mparij@gmail.com>
2023-05-15 11:24:23 -0300

    move to esm module only

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,7 @@ export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/cjs/index.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/esm/index.js",
+      file: "dist/index.js",
       format: "esm",
       sourcemap: true,
     },
